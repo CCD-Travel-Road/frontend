d30d300f65205927ae8a09d6fb8b24ebd3fa029b
ming0o <isha3992@gmail.com>
2024-07-10 19:18:59 +0900

    test: eslint, prettier 수정 및 카카오지도 api 테스팅 완료

---
+++ src/pages/MapPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Map from '../components/map'
+
+function MapPage() {
+    return (
+        <div>
+            <h1>Kakao 지도 시작하기</h1>
+            <Map />
+        </div>
+    );
+}
+
+
+export default MapPage
