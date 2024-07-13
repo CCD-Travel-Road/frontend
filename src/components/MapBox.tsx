c615d1db085a3606149eac1f68d3737065579ebd
ming0o <isha3992@gmail.com>
2024-07-13 16:43:32 +0900

    fix: TypeError 해결

--- src/components/MapBox.tsx
+++ src/components/MapBox.tsx
@@ -9,6 +9,7 @@ declare global {
 
 interface Kakao {
     maps: {
+        load(initializeMap: () => void): unknown;
         Map: new (container: HTMLElement | null, options: MapOptions) => KakaoMap;
         LatLng: new (lat: number, lng: number) => LatLng;
     };
@@ -32,31 +33,33 @@ interface KakaoMap {
 const MapBox = () => {
     useEffect(() => {
         const initializeMap = () => {
-            const container = document.getElementById('map');
-            if (container && window.kakao && window.kakao.maps) {
-                const options = {
-                    center: new window.kakao.maps.LatLng(37.517223, 127.041292),
-                    level: 3,
-                };
+            if (window.kakao && window.kakao.maps) {
+                const container = document.getElementById('map');
+                if (container) {
+                    const options = {
+                        center: new window.kakao.maps.LatLng(37.517223, 127.041292),
+                        level: 3,
+                    };
 
-                const map = new window.kakao.maps.Map(container, options);
-                console.log(map);
+                    const map = new window.kakao.maps.Map(container, options);
+                    console.log(map);
+                }
             } else {
                 console.error("Kakao Maps API is not available.");
             }
         };
 
         if (window.kakao && window.kakao.maps) {
-            initializeMap();
+            window.kakao.maps.load(initializeMap);
         } else {
             const mapScript = document.createElement('script');
             mapScript.async = true;
-            mapScript.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_APP_KEY}&libraries=services,clusterer`;
+            mapScript.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_APP_KEY}&autoload=false&libraries=services,clusterer`;
             document.head.appendChild(mapScript);
 
             mapScript.onload = () => {
                 if (window.kakao && window.kakao.maps) {
-                    initializeMap();
+                    window.kakao.maps.load(initializeMap);
                 } else {
                     console.error("Failed to load Kakao Maps API.");
                 }
