1f1cebb79a59a19028a32783dc84c72e650d932d
Kim Hee Chan <a01049984394k@gmail.com>
2024-07-17 15:47:49 +0900

    feat : 메인페이지 퍼블리싱
    진행 중

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,24 +1,48 @@
 import React from "react";
-import styled from "styled-components";
 
 // components
 import Navigation from "src/components/Navigation/Navigation";
 import MainHeader from "src/components/Header/MainHeader";
+import MainTitle from "src/components/MainTitle";
 
+import * as T from './Styles';
 
-const Wrapper = styled.div`
-    width: 100vw;
-`;
+// image
+import bannerElementA from "../../images/mainPage/main-banner-elementA.png"
 
 function MainPage() {
 
     return (
 
-        <Wrapper>
+        <T.EntireContainer>
             <MainHeader/>
 
+            {/* 메인페이지 배너 */}
+            <T.BannerFrame>
+
+                {/* 유저관련 텍스트 프레임 */}
+                <T.UserTextFrame>
+                    <T.Title>안녕하세요 김희찬님</T.Title>
+                    <T.SubTitle>새로운 여행을 떠나보세요!</T.SubTitle>
+
+                    <T.UserInteractionBtn>
+                        <T.BtnText>여행 만들기</T.BtnText>
+                    </T.UserInteractionBtn>
+                </T.UserTextFrame>
+
+                {/* UI용 일러스트 이미지 프레임 */}
+                <T.UIImage src={bannerElementA} />
+
+            </T.BannerFrame>
+
+            {/* 콘텐츠 */}
+            <T.ContentFrame>
+                <MainTitle content="요즘 인기많은 여행 코스에요."/>
+            </T.ContentFrame>
+            
+
             <Navigation/>
-        </Wrapper>
+        </T.EntireContainer>
 
     );
 }
