75ec6176398512d1101800a7ce7f97fd3927e2ef
Kim Hee Chan <a01049984394k@gmail.com>
2024-07-18 00:09:22 +0900

    feat : 하단 네비게이션 조건부 활성화
    240718
    하단 네비게이션 현재 페이지 위치에 따라 활성화 영역 기능

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import MapPage from './pages/MapPage';
 import MainPage from './pages/MainPage/MainPage';
 import ASetDate from './pages/CreateJourneyPage/ASetDate';
 
+import ViewJourney from './pages/ViewJourneyPage/ViewJourney';
+
+import Community from './pages/CommunityPage/Community'
+
 function App() {
   return (
     <RecoilRoot>
@@ -13,6 +17,8 @@ function App() {
         <Route path="/Map" element={<MapPage />} />
         <Route path="/" element={<MainPage />} />
         <Route path="/setDate" element={<ASetDate />} />
+        <Route path="/view" element={<ViewJourney />} />
+        <Route path="/community" element={<Community />} />
       </Routes>
     </RecoilRoot>
   );
