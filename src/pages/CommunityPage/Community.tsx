17dbf6c253ff8ca28c5833cb45ce7d512e6017bb
76Hee <105695940+76Hee@users.noreply.github.com>
2024-08-27 02:19:34 +0900

    feat: 커뮤니티 퍼블리싱

--- src/pages/CommunityPage/Community.tsx
+++ src/pages/CommunityPage/Community.tsx
@@ -1,16 +1,32 @@
-// components
+import { useState } from "react";
+
 import Navigation from "src/components/Navigation/Navigation";
 import FunctionHeader from "src/components/Header/FunctionHeader";
+import SearchBar from "src/components/SearchBar";
+
 import * as T from "./Styles"
 
 
 function Community() {
 
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+
     return (
 
         <T.EntireContainer>
 
-            <FunctionHeader content="여행 조회"/>
+            <FunctionHeader content="커뮤니티"/>
+            <T.ContentsFrame>
+                <SearchBar placeholder="검색어를 입력하세요." />
+                <T.CommunityFilter type="select" value={selectedOption} onChange={handleChange}>
+                    <T.OptionFilter value="옵션 1">옵션 1</T.OptionFilter>
+                </T.CommunityFilter>
+            </T.ContentsFrame>
+
             <Navigation />
 
         </T.EntireContainer>
--- src/pages/CommunityPage/Styles.ts
+++ src/pages/CommunityPage/Styles.ts
@@ -3,3 +3,24 @@ import styled from 'styled-components';
 export const EntireContainer = styled.div`
     width: 100vw;
 `;
+
+export const ContentsFrame = styled.div`
+    width: 100%;
+    padding:12px 20px 0px 20px;
+
+    background:white;
+`;
+
+export const CommunityFilter = styled.input`
+    width: 100%;
+    padding:12px 20px 0px 20px;
+
+    background:white;
+`;
+
+export const OptionFilter = styled.option`
+    width: 100%;
+    padding:12px 20px 0px 20px;
+
+    background:white;
+`;
