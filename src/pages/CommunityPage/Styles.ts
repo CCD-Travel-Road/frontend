9d0626fdb292c97e26b0973279dd0df1f4f9e11d
76Hee <105695940+76Hee@users.noreply.github.com>
2024-08-28 00:46:40 +0900

    stlye: 커뮤니티 페이지 퍼블리싱
    1. 필터링 추가

--- src/pages/CommunityPage/Styles.ts
+++ src/pages/CommunityPage/Styles.ts
@@ -6,21 +6,32 @@ export const EntireContainer = styled.div`
 
 export const ContentsFrame = styled.div`
     width: 100%;
+    min-height:100vh;
     padding:12px 20px 0px 20px;
 
     background:white;
 `;
 
-export const CommunityFilter = styled.input`
-    width: 100%;
-    padding:12px 20px 0px 20px;
+export const SelectFrame = styled.div`
+    display:flex;
+    align-items:center;
 
+    width:100%;
+    gap:12px;
+    margin-bottom:40px;
+`;
+
+export const CommunityFilter = styled.select`
+    border-radius:50px;
+    border:1px solid #E7E7E7;
+
+    padding:12px 16px;
     background:white;
+    color:var(--textColor);
 `;
 
 export const OptionFilter = styled.option`
-    width: 100%;
-    padding:12px 20px 0px 20px;
-
-    background:white;
+    width:fit-content;
+    font-size:12px;
+    font-weight: 400;
 `;
